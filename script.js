af70a8fadf101c513d52b8dab70700628584cba1
jbenisty <jonathan.benisty@gmail.com>
2020-12-08 13:20:03 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+
+const generateTarget = () => {
+    return Math.floor(Math.random() *10);
+};
+
+const compareGuesses = (currentHumanGuess, computerGuess, target) => {
+    let human = (target - currentHumanGuess) + 100; 
+    let computer = (target - computerGuess) + 100; 
+    if ((human < computer) || (human === computer)) {
+        return true;
+    } else if (computer < human) {
+        return false;
+    } 
+};
